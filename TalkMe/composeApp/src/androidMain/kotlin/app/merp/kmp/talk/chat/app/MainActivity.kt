4338fa9457288b2e738f272b5e9a61b4e98df40d
PremDanej <danejprem005@gmail.com>
2025-06-26 15:48:49 +0530

    Taking username feature added.

--- TalkMe/composeApp/src/androidMain/kotlin/app/merp/kmp/talk/chat/app/MainActivity.kt
+++ TalkMe/composeApp/src/androidMain/kotlin/app/merp/kmp/talk/chat/app/MainActivity.kt
@@ -4,9 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.lifecycleScope
 import app.merp.kmp.talk.chat.app.ui.theme.TalkAppTheme
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import java.net.SocketException
+import java.net.SocketTimeoutException
+
+const val PORT = BuildConfig.PORT
+const val HOST = BuildConfig.HOST
 
 class MainActivity : ComponentActivity() {
 
@@ -14,15 +24,35 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        lifecycleScope.launch {
-            chatClient.connect("ws://192.168.196.89:8000/ws/chat")
-        }
-
         enableEdgeToEdge()
         setContent {
+
+            var username by remember { mutableStateOf("") }
+            var connected by remember { mutableStateOf(false) }
+
             TalkAppTheme {
-                App(chatClient = chatClient)
+                if (!connected) {
+                    UsernameInput { gettingUsername ->
+                        username = gettingUsername
+                        lifecycleScope.launch {
+                            try {
+                                chatClient.connection(
+                                    url = "ws://$HOST:$PORT/ws/chat/$username",
+                                    username = username
+                                )
+                                connected = true
+                            } catch (e: SocketException) {
+                                println("TALK -> SocketException || Failed to connect -> ${e.message}")
+                            } catch (e: SocketTimeoutException) {
+                                println("TALK -> SocketTimeOutError || Failed to connect -> ${e.message}")
+                            } catch (e: Exception) {
+                                println("TALK -> Connection error -> ${e.message}")
+                            }
+                        }
+                    }
+                } else {
+                    App(chatClient = chatClient)
+                }
             }
         }
     }
